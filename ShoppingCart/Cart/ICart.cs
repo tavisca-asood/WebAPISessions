87d389c842e3a6891e87adc031a8d18ae6751ddd
tavisca-asood <asood@tavisca.com>
2018-09-04 10:40:41 +0530

    Added files for Assignment1 - Simple shopping cart implementation

---
+++ ShoppingCart/Cart/ICart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cart
+{
+    interface ICart
+    {
+        void AddItemToCart(CartItem item);
+        void RemoveItemFromCart(int ItemId);
+        double TotalAmount();
+        void ShowCart();
+        void ClearCart();
+        void Checkout();
+    }
+}
